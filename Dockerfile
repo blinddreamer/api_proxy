5bb589e86120f1a7668c96d6ab20d063e817a68c
Nikolay Apostolov <blinddreamer.huku.rocks>
2023-08-04 05:10:06 +0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use the official Node.js image as the base image
+FROM node:14
+
+# Create and set the working directory inside the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the server code to the working directory
+COPY . .
+
+# Expose the port that the server listens on (5000 in this case)
+EXPOSE 5000
+
+# Start the server when the container runs
+CMD ["npm", "start"]
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 5000; // Choose any available port
+
+// CORS configuration: Replace 'your-frontend-domain' with your actual frontend domain
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
+app.use(cors(corsOptions));
+
+// Define a route to handle the proxy request
+app.get('/api/monitors', async (req, res) => {
+  try {
+    // Use dynamic import() instead of require() for node-fetch
+    const fetch = await import('node-fetch');
+
+    const apiUrl = "https://api.hetrixtools.com/v1/e1546211de0f332ec510d8a85c9461e2/uptime/monitors/0/30/";
+    const response = await fetch.default(apiUrl);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Proxy Error:", error);
+    res.status(500).json({ error: 'An error occurred while fetching data from the API.' });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Proxy server listening at http://localhost:${port}`);
+});
